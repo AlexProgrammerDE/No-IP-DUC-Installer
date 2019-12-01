c6840f822c39aba18c96bfaf4ab42d0b46f6b036
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-12-01 16:20:10 +0100

    Fix installation error.

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ sudo apt update
 sudo apt install -y make gcc
 
 # Instructions of the no ip website
-sudo cd /usr/local/src
+cd /usr/local/src
 sudo wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
 sudo tar xzf noip-duc-linux.tar.gz
-sudo cd noip-2.1.9-1
+cd noip-2.1.9-1
 sudo make
 sudo make install
