21a38cd22634928118f51a1b4ad3e20ebf6bed0b
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-12-01 16:21:34 +0100

    Fix error

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ sudo apt install -y make gcc
 
 # Instructions of the no ip website
 cd /usr/local/src
-sudo wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
+sudo wget -O noip-duc-linux.tar.gz http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
 sudo tar xzf noip-duc-linux.tar.gz
+sudo rm noip-duc-linux.tar.gz
 cd noip-2.1.9-1
 sudo make
 sudo make install
